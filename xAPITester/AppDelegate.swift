3f8b87a3de839607199b17fef9b3748e1c460f83
Douglas Adams <douglas.adams@me.com>
2020-01-16 19:28:30 -0500

    Initial Commit

---
+++ xAPITester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xAPITester
+//
+//  Created by Douglas Adams on 1/16/20.
+//  Copyright © 2020 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
